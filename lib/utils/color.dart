0a57b5fbacd43b78a76978492066e89fe9ee8465
selinkeles <selinkeles@sabanciuniv.edu>
2021-11-29 21:24:19 +0300

    selin

--- lib/utils/color.dart
+++ lib/utils/color.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 class AppColors {
 
   //login
-  static const Color loginBackTop = const Color(0xFF000000);
-  static const Color loginBackBottom = const Color(0xFF0040FE);
-  static const Color TextFormBg = const Color(0xFF5972AA);
+  static const Color loginBackTop = const Color(0xFF000000); //var
+  static const Color loginBackBottom = const Color(0xFF0040FE); //var
+  static const Color TextFormBg = const Color(0xFF5972AA);  //var
 
 
   static const Color textColor = const Color(0xFF424242);
-  static const Color headingColor = const Color(0xFF0277BD);
+  static const Color headingColor = const Color(0xFF0277BD); //var
   static const Color captionColor = Colors.white30;
 }
