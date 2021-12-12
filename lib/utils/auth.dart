eda4f483abe7398d918d8d50a8156814878b55d5
serhanyrlmz <70518544+serhanyrlmz@users.noreply.github.com>
2021-12-12 22:49:15 +0300

    serhan auth
    serhan auth

--- lib/utils/auth.dart
+++ lib/utils/auth.dart
@@ -1,7 +1,14 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
+import 'package:UniPath/utils/database.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final googleSignIn = GoogleSignIn();
 
   User? _userFromFirebase(User? user) {
     return user ?? null;
@@ -11,30 +18,117 @@ class AuthService {
     return _auth.authStateChanges().map(_userFromFirebase);
   }
 
-  Future signInAnon() async {
+  Future signInAnon(StackTrace stackTrace) async {
     try {
       UserCredential result = await _auth.signInAnonymously();
       User user = result.user!;
+      return _userFromFirebase(user);
     } catch (e) {
+      await FirebaseCrashlytics.instance.recordError(
+        e,
+        stackTrace,
+        reason: e.toString(),
+      );
       print(e.toString());
       return null;
     }
   }
 
-  Future signupWithMailAndPass(String mail, String pass) async {
+  Future getUserCredentials() async {
+    final GoogleSignInAccount? googleUser = await googleSignIn.signIn();
+
+    // Obtain the auth details from the request
     try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(email: mail, password: pass);
-      User user = result.user!;
+      final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      return [credential, googleUser!.email];
+    } on PlatformException catch (e) {
+      print(e);
+    }
+  }
+
+  Future signInWithGoogle() async {
+    try {
+      // Trigger the authentication flow
+      final credential = await getUserCredentials();
+
+      // Once signed in, return the UserCredential
+      return await FirebaseAuth.instance.signInWithCredential(credential[0]);
+    } on FirebaseAuthException catch (e) {
+      await FirebaseCrashlytics.instance.recordError(
+        e,
+        StackTrace.current,
+        reason: e.toString(),
+      );
+      FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled;
+      //googleSignIn.disconnect();
+      return e.message;
     } catch (e) {
+      await FirebaseCrashlytics.instance.recordError(
+        e,
+        StackTrace.current,
+        reason: e.toString(),
+      );
+      FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled;
+      //googleSignIn.disconnect();
+    }
+  }
+
+  Future signupWithMailAndPass(String mail, String pass, String name,
+      String surname, String username) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(
+          email: mail, password: pass);
+      User user = result.user!;
+
+      // Add user to database before returning to profile
+      DBService dbService = DBService();
+      String userToken = await user.uid;
+      dbService.addUser(name, surname, mail, userToken, username, pass);
+
+      return _userFromFirebase(user);
+    } on FirebaseAuthException catch (e) {
+      await FirebaseCrashlytics.instance.recordError(
+        e,
+        StackTrace.current,
+        reason: e.toString(),
+      );
+      //FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled;
+      return e.code.toString();
+    } catch (e) {
+      await FirebaseCrashlytics.instance.recordError(
+        e,
+        StackTrace.current,
+        reason: e.toString(),
+      );
+      //FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled;
       print(e.toString());
-      return null;
+      String message = e.toString();
+      return message;
     }
   }
 
   Future loginWithMailAndPass(String mail, String pass) async {
     try {
-      UserCredential result = await _auth.signInWithEmailAndPassword(email: mail, password: pass);
+      UserCredential result =
+      await _auth.signInWithEmailAndPassword(email: mail, password: pass);
       User user = result.user!;
+      return _userFromFirebase(user);
+    } on FirebaseAuthException catch (e) {
+      await FirebaseCrashlytics.instance.recordError(
+        e,
+        StackTrace.current,
+        reason: e.toString(),
+      );
+      FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled;
+      return e.code.toString();
     } catch (e) {
       print(e.toString());
       return null;
@@ -43,10 +137,37 @@ class AuthService {
 
   Future signOut() async {
     try {
+      if (await googleSignIn.isSignedIn()) {
+        await googleSignIn.disconnect();
+        await googleSignIn.signOut();
+      }
       return await _auth.signOut();
     } catch (e) {
+      await FirebaseCrashlytics.instance.recordError(
+        e,
+        StackTrace.current,
+        reason: e.toString(),
+      );
+      FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled;
       print(e.toString());
       return null;
+    }
+  }
+
+  Future deleteAccount() async {
+    try {
+      await FirebaseAuth.instance.currentUser!.delete();
+    } on FirebaseAuthException catch (e) {
+      await FirebaseCrashlytics.instance.recordError(
+        e,
+        StackTrace.current,
+        reason: e.toString(),
+      );
+      FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled;
+      if (e.code == 'requires-recent-login') {
+        print(
+            'The user must reauthenticate before this operation can be executed.');
+      }
     }
   }
 }
