1cf522baa6b44031d393f77e3ec4f2e34ca244b6
aenes22346 <95237883+aenes22346@users.noreply.github.com>
2021-12-12 18:16:54 +0300

    Add files via upload

---
+++ lib/utils/auth.dart
@@ -0,0 +1,52 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User? _userFromFirebase(User? user) {
+    return user ?? null;
+  }
+
+  Stream<User?> get user {
+    return _auth.authStateChanges().map(_userFromFirebase);
+  }
+
+  Future signInAnon() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User user = result.user!;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signupWithMailAndPass(String mail, String pass) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: mail, password: pass);
+      User user = result.user!;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future loginWithMailAndPass(String mail, String pass) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: mail, password: pass);
+      User user = result.user!;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+}
---
+++ lib/utils/database.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Database {
+
+  final String uid;
+  Database({this.uid});
+
+  final CollectionReference userCollection = FirebaseFirestore.instance.collection('users');
+
+  Future<void> createUserData(String username, String phoneNumber) async {
+    await userCollection.doc(uid).set({
+      'username': username,
+      'phoneNumber': phoneNumber,
+      'postCount': 0,
+      'followersCount': 0,
+      'followingCount': 0,
+      'bio': '',
+      'usersPosts': [],
+      'userFollowers': [],
+      'userFollowing': [],
+      'usersChats': [],
+      'isPrivate': false,
+      'isDeactivated': false,
+      'profilePhoto': '',
+      'profilePhotoUrl': '',
+    });
+  }
+}
