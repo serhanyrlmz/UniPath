f255f32767d964bd1ecc47b642ab11c66e5b29ac
gizemkulekcii <gizemkulekcii@gmail.com>
2021-12-22 22:50:23 +0300

    gizem

--- lib/routes/login.dart
+++ lib/routes/login.dart
@@ -3,18 +3,20 @@ import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
 
+
+
+
 class Login extends StatefulWidget {
-  const Login({Key? key, required this.analytics, required this.observer}) : super(key: key);
+  const Login({Key? key, required this.analytics, required this.observer})
+      : super(key: key);
 
   final FirebaseAnalytics analytics;
   final FirebaseAnalyticsObserver observer;
-
   @override
   _LoginState createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
-
 
   @override
   Widget build(BuildContext context) {
@@ -28,113 +30,108 @@ class _LoginState extends State<Login> {
       return Colors.black;
     }
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Container(
+      body: Container(
           decoration: BoxDecoration(
             gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end :Alignment.bottomCenter,
-              colors: [AppColors.loginBackTop, AppColors.loginBackBottom]
+                begin: Alignment.topCenter,
+                end :Alignment.bottomCenter,
+                colors: [AppColors.loginBackTop, AppColors.loginBackBottom]
             ),
           ),
-
           child: Center(
-            child:Column(
-              children:[
-                  Spacer(flex:9),
-                  Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
+              child: Column(
                   children:[
+                    Spacer(flex:5),
+
+                    IconButton(
+                      icon: Icon(Icons.add_a_photo_rounded),
+                      iconSize:125,
+                      color:Colors.white,
+                      onPressed:(){
+                        //add the choosing photo function here.
+                      },
+                    ),
+
+                    Spacer(flex:3),
+
                     Padding(
-                      padding: EdgeInsets.fromLTRB(0, 0, 0, 0),
-                      child: CircleAvatar(
-                        radius: 125,
-                        child: ClipOval(
-                          child:Image.asset('assets/logo.jpeg',
-                            width: MediaQuery.of(context).size.width)
-                        )
-                      )
-                    ),
-                  ],
-                ),
-
-                Spacer(flex:5),
-
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(22,0,22,10),
-                  child:
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      fillColor: AppColors.TextFormBg,
-                      filled: true,
-                      hintText: 'Email',
-                      prefixIcon: Icon(Icons.email_outlined),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
+                      padding: const EdgeInsets.fromLTRB(22,0,22,10),
+                      child:
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          fillColor: AppColors.TextFormBg,
+                          filled: true,
+                          hintText: 'Email',
+                          prefixIcon: Icon(Icons.email_outlined),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(320)),
+                          ),
                         ),
-                        borderRadius: BorderRadius.all(Radius.circular(320)),
+                        keyboardType: TextInputType.text,
+                        obscureText: false,
+                        enableSuggestions: false,
+                        autocorrect: false,
                       ),
                     ),
-                    keyboardType: TextInputType.text,
-                    obscureText: false,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                  ),
 
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(22,10,22,0),
-                  child:
-                  TextFormField(
-                    decoration: const InputDecoration(
-                      fillColor: AppColors.TextFormBg,
-                      filled: true,
-                      hintText: 'Password',
-                      prefixIcon: Icon(Icons.vpn_key_outlined),
-                      border: OutlineInputBorder(
-                        borderSide: BorderSide(
-                          color: AppColors.TextFormBg,
+
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(22,10,22,0),
+                      child:
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          fillColor: AppColors.TextFormBg,
+                          filled: true,
+                          hintText: 'Password',
+                          prefixIcon: Icon(Icons.vpn_key_outlined),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.TextFormBg,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                          ),
                         ),
-                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                        keyboardType: TextInputType.text,
+                        obscureText: true,
+                        enableSuggestions: false,
+                        autocorrect: false,
                       ),
                     ),
-                    keyboardType: TextInputType.text,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                  ),
-                ),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/forgot_pass');},
-                    child:
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/forgot_pass');},
+                      child:
                       Text("Forgot password?",),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(0,10,0,0),
-                  child: ElevatedButton(
-                    child: Text("Login", style: TextStyle(fontSize: 20.0),),
-                    //color: Colors.red,
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/HomePage');},
-                  ),
-                ),
-                Spacer(flex:10),
-                TextButton(
-                  child: Text("Don't have an account? Sign Up!", style: TextStyle(fontSize:20.0),),
-                  style: ButtonStyle(overlayColor:
-                  MaterialStateProperty.resolveWith(getColor),),
-                  onPressed: () {
-                    Navigator.pushNamed(context, "/signup");
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.fromLTRB(0,10,0,0),
+                      child: ElevatedButton(
+                        child: Text("Login", style: TextStyle(fontSize: 20.0),),
+                        //color: Colors.red,
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/HomePage');},
+                      ),
+                    ),
+
+
+                    Spacer(flex:10),
+
+                    TextButton(
+                      child: Text("Don't have an account? Sign Up!", style: TextStyle(fontSize:20.0),),
+                      style: ButtonStyle(overlayColor:
+                      MaterialStateProperty.resolveWith(getColor),),
+                      onPressed: () {
+                        Navigator.pushNamed(context, "/signup");
+                      },
+                    ),
+                  ]
+              )
+          )
       ),
     );
   }
 }
+
