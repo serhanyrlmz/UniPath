a1f837bce037cbb957092efa6d307e67059a8fe3
aenes22346 <95237883+aenes22346@users.noreply.github.com>
2021-12-26 19:14:46 +0300

    Create profile.dart
    profile of user

---
+++ lib/routes/profile.dart
@@ -0,0 +1,272 @@
+import 'package:flutter/material.dart';
+import 'package:UniPath/utils/color.dart';
+import 'package:UniPath/utils/styles.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:UniPath/utils/analytics.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'profile.dart';
+import 'package:UniPath/routes/EditProfile.dart';
+import 'package:UniPath/utils/database.dart';
+import 'package:UniPath/routes/HomePage.dart';
+import 'package:UniPath/utils/user.dart';
+
+class Profile extends StatefulWidget {
+  final String? profileId;
+
+  Profile({this.profileId});
+
+  @override
+  _ProfileState createState() => _ProfileState();
+}
+
+class _ProfileState extends State<Profile> {
+  final String currentUserId = currentUser!.id.toString();
+  bool isLoadingPost = false;
+  int postCount = 0;
+  //List<Post>? posts = [];
+
+  //post orientation
+  String postOrientation = "grid";
+  @override
+  void initState() {
+    super.initState();
+    //getProfilePost();
+  }
+
+  /*getProfilePost() async {
+    setState(() {
+      isLoadingPost = true;
+    });
+
+    QuerySnapshot snapshot = await postReference
+        .doc(widget.profileId)
+        .collection("userPost")
+        .orderBy("timestamp", descending: true)
+        .get();
+    setState(() {
+      isLoadingPost = false;
+      postCount = snapshot.docs.length;
+      posts = snapshot.docs.map((e) => Post.fromDocument(e)).toList();
+      print(posts);
+    });
+  }
+  */
+
+//edit profile function
+  editProfile() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditProfile(
+          currentUserId: currentUserId,
+        ),
+      ),
+    );
+  }
+
+//build button for profile
+  buildButton({String? text, function}) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: GestureDetector(
+        onTap: function,
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          width: MediaQuery.of(context).copyWith().size.width / 3,
+          decoration: BoxDecoration(
+              color: Theme.of(context).primaryColor,
+              borderRadius: BorderRadius.circular(5.0)),
+          child: Center(
+            child: Text(
+              text!,
+              //textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 18),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+//to show edit profile button
+  buildProfileButton() {
+    bool isProfileOwner = currentUserId == widget.profileId;
+    if (isProfileOwner) {
+      return buildButton(text: "Edit  Profile", function: editProfile);
+    }
+    return Text("profile button");
+  }
+
+  //to show post level and count
+  Column buildCountColumn(String lavel, int count) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Text(
+          count.toString(),
+          style: TextStyle(fontWeight: FontWeight.w400, fontSize: 22.0),
+        ),
+        Container(
+          child: Text(
+            lavel.toString(),
+            style: TextStyle(
+                fontWeight: FontWeight.w400, fontSize: 17, color: Colors.grey),
+          ),
+        )
+      ],
+    );
+  }
+
+//to show profile header
+  buildProfileHeader() {
+    return FutureBuilder(
+      future: userReference.doc(widget.profileId).get(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        UserModel user = UserModel.fromDocument(snapshot.data);
+        return Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: NetworkImage(user.photoUrl.toString()),
+                  ),
+                  Expanded(
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              buildCountColumn("post", postCount),
+                              buildCountColumn("followers", 0),
+                              buildCountColumn("following", 1),
+                            ],
+                          ),
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [buildProfileButton()],
+                          )
+                        ],
+                      ))
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 15.0),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  user.username.toString(),
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 8.0),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  user.displayname.toString(),
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  /*buildProfilePost() {
+    if (isLoadingPost) {
+      //return circularProgress();
+    } else if (posts!.isEmpty) {
+      return Padding(
+        padding: const EdgeInsets.all(30),
+        child: Container(
+          child: Center(
+            child: Text("no post avilable \n   upload post",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22)),
+          ),
+        ),
+      );
+    } else if (postOrientation == "grid") {
+      List<GridTile>? gridTile = [];
+      posts!.forEach((val) {
+        gridTile.add(
+          GridTile(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+            ),
+          ),
+        );
+      });
+
+      return GridView.count(
+        crossAxisCount: 3,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 2,
+        shrinkWrap: true,
+        physics: NeverScrollableScrollPhysics(),
+        children: gridTile,
+      );
+    }
+  }
+  */
+
+  setPostOrientation(String orientation) {
+    setState(() {
+      this.postOrientation = orientation;
+    });
+  }
+
+  buildTooglePostOrientation() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        IconButton(
+          onPressed: () => setPostOrientation("grid"),
+          icon: Icon(Icons.grid_on),
+          color: postOrientation == "grid"
+              ? Theme.of(context).primaryColor
+              : Colors.grey,
+        ),
+
+        IconButton(
+          onPressed: () => setPostOrientation("list"),
+          icon: Icon(Icons.list),
+          color: postOrientation == "list"
+              ? Theme.of(context).primaryColor
+              : Colors.grey,
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          buildProfileHeader(),
+          Divider(
+            height: 10.0,
+            color: Colors.grey,
+          ),
+          buildTooglePostOrientation(),
+          Divider(
+            height: 10.0,
+            color: Colors.grey,
+          ),
+          //buildProfilePost(),
+        ],
+      ),
+    );
+  }
+}
